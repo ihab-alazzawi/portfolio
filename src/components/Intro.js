b0d3296cc9c7ef42258dc11d204ef36a6ff40467
Ihab-AL <ihab.alazzawi@outlook.com>
2018-09-10 12:16:54 -0500

    add logo and raleway font to intro

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,31 +4,29 @@ import { Link } from 'react-router-dom';
 
 const Intro = () => {
   return (
-    <div>
-      <div className="row">
-        {' '}
+    <div className="intro">
+      <div className="intro-logo">
+        <h1 className="logo-text">IA</h1>
+      </div>
+      <div>
         <Link to="/" className="btn-landing">
           Home
-        </Link>{' '}
+        </Link>
       </div>
       <div>
-        {' '}
         <Link to="/projects" className="btn-projects">
           Projects
-        </Link>{' '}
+        </Link>
       </div>
       <div>
-        {' '}
         <Link to="/about" className="btn-about">
           About
-        </Link>{' '}
+        </Link>
       </div>
       <div>
-        {' '}
         <Link to="/contact" className="btn-contact">
           Contact
-          <span />
-        </Link>{' '}
+        </Link>
       </div>
     </div>
   );
