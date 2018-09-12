f5461915fb6e04e0dbc9c63a69b31b811520ec53
Ihab-AL <ihab.alazzawi@outlook.com>
2018-09-12 09:35:16 -0500

    add show/hide toggle with animations to Intro

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,35 +1,85 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../assets/styles/Intro.css';
 import { Link } from 'react-router-dom';
 
-const Intro = () => {
-  return (
-    <div className="intro">
-      <div className="intro-logo">
-        <h1 className="logo-text">IA</h1>
-      </div>
+class Intro extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: undefined
+    };
+    this.handleSlideOut = this.handleSlideOut.bind(this);
+    this.handleSlideIn = this.handleSlideIn.bind(this);
+  }
+
+  handleSlideOut() {
+    this.setState({ visible: true });
+  }
+
+  handleSlideIn() {
+    this.setState({ visible: false });
+  }
+
+  render() {
+    const { visible } = this.state;
+    return (
       <div>
-        <Link to="/" className="btn-landing">
-          Home
-        </Link>
+        <div className="close-btn" onClick={this.handleSlideIn} />
+        <div onClick={this.handleSlideOut}>
+          <Link
+            to="/"
+            className={
+              visible
+                ? 'animated fadeOutUpBig slower intro-logo'
+                : 'animated fadeInDown intro-logo'
+            }
+          >
+            <h1 className="logo-text">IA</h1>
+          </Link>
+          <Link
+            to="/"
+            className={
+              visible
+                ? 'animated fadeOutUpBig slower btn-landing'
+                : 'animated fadeInDown btn-landing'
+            }
+          >
+            Home
+          </Link>
+          <Link
+            to="/projects"
+            className={
+              visible
+                ? 'animated fadeOutUpBig slower btn-projects'
+                : 'animated fadeInDown btn-projects'
+            }
+          >
+            Projects
+          </Link>
+          <Link
+            to="/about"
+            className={
+              visible
+                ? 'animated fadeOutUpBig slower btn-about'
+                : 'animated fadeInDown btn-about'
+            }
+          >
+            About
+          </Link>
+          <Link
+            to="/contact"
+            className={
+              visible
+                ? 'animated fadeOutUpBig slower btn-contact'
+                : 'animated fadeInDown btn-contact'
+            }
+          >
+            Contact
+          </Link>
+        </div>
       </div>
-      <div>
-        <Link to="/projects" className="btn-projects">
-          Projects
-        </Link>
-      </div>
-      <div>
-        <Link to="/about" className="btn-about">
-          About
-        </Link>
-      </div>
-      <div>
-        <Link to="/contact" className="btn-contact">
-          Contact
-        </Link>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Intro;
