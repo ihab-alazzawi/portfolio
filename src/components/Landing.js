2a5e6f25dffdf1673a0d66a7964261fe95208b7e
Ihab-AL <ihab.alazzawi@outlook.com>
2018-09-19 09:53:56 -0500

    Add functionality and markup to Landing

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,12 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../assets/styles/Landing.css';
+import Icons from './Icons';
+import Header from './Header';
 
-const Landing = () => {
-  return (
-    <div className="landing-main">
-      <h1>Landing</h1>
-    </div>
-  );
-};
+class Landing extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      scrollTo: false
+    };
+    this.handleScroll = this.handleScroll.bind(this);
+  }
+
+  handleScroll() {
+    this.setState(prevState => ({
+      scrollTo: !prevState.scrollTo
+    }));
+  }
+  render() {
+    const { scrollTo } = this.state;
+    return (
+      <scroll-container>
+        <scroll-page id="landing" className="landing-item top">
+          <Header />
+          <h1 className="start">
+            H<span>I</span>, <span>I</span>'<span>M</span> I<span>H</span>A
+            <span>B</span>
+          </h1>
+          <h2>FULL-STACK JAVASCRIPT ENGINEER</h2>
+        </scroll-page>
+        <div id={scrollTo ? 'scroll-bot' : 'scroll-top'} className="scrl">
+          <a
+            onClick={this.handleScroll}
+            href={scrollTo ? '#landing' : '#skills'}
+          >
+            <span />
+            <span />
+            <span />
+          </a>
+        </div>
+        <scroll-page id="skills" className="landing-item bottom">
+          <div className="flex-container">
+            <h1>SKILLS</h1>
+            <div className="flex-item">
+              <Icons />
+            </div>
+          </div>
+        </scroll-page>
+      </scroll-container>
+    );
+  }
+}
 
 export default Landing;
