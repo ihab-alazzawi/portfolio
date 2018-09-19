a4eca62d380aa233340d5324528fe1181cee5e48
Ihab-AL <ihab.alazzawi@outlook.com>
2018-09-19 09:52:05 -0500

    Add Header content

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import '../assets/styles/Header.css';
 
-const Header = () => {
-  return <div>Header</div>;
-};
+const Header = () => (
+  <header>
+    <div>
+      <i className="fas fa-envelope fa-2x" />
+      <i className="fab fa-linkedin fa-2x" />
+      <i className="fab fa-facebook fa-2x" />
+      <i className="fas fa-cloud-download-alt fa-2x">
+        <p className="resume">RESUME</p>
+      </i>
+    </div>
+  </header>
+);
 
 export default Header;
