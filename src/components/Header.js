dbae0950ee2d0fb83e9c97b91f08581e8c4d3519
Ihab-AL <ihab.alazzawi@outlook.com>
2018-09-27 17:21:46 -0500

    Add links to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,29 @@ import '../assets/styles/Header.css';
 const Header = () => (
   <header>
     <div>
-      <i className="fas fa-envelope fa-2x" />
-      <i className="fab fa-linkedin fa-2x" />
-      <i className="fab fa-facebook fa-2x" />
-      <i className="fas fa-cloud-download-alt fa-2x">
-        <p className="resume">RESUME</p>
-      </i>
+      <a href="mailto:info@ihab.io?subject=Hi%20there!">
+        <i className="fas fa-envelope fa-2x" />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/ihab-alazzawi"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i className="fab fa-linkedin fa-2x" />
+      </a>
+      <a
+        href="https://www.facebook.com/ihab.dev"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i className="fab fa-facebook fa-2x" />
+      </a>
+      <a href="Resume.pdf" target="_blank" rel="noopener noreferrer">
+        {' '}
+        <i className="fas fa-cloud-download-alt fa-2x">
+          <p className="resume">RESUME</p>
+        </i>
+      </a>
     </div>
   </header>
 );
