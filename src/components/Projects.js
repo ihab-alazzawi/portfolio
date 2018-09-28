91f3e50e51ba7d477096060cda9a1fb37e3611d8
Ihab-AL <ihab.alazzawi@outlook.com>
2018-09-28 13:15:05 -0500

    Rebuilt Projects component with new content

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,75 +1,115 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import '../assets/styles/Projects.css';
-import '../../node_modules/react-image-gallery/styles/css/image-gallery.css';
-import devNetwork from '../assets/img/projects/devnetwork.png';
-import portfolio from '../assets/img/projects/portfolio.png';
-import weather from '../assets/img/projects/weather.png';
-import ImageGallery from 'react-image-gallery';
 
-class Projects extends PureComponent {
-  render() {
-    const dn = (
-      <a href="https://dnetwork.herokuapp.com" target="_blank">
-        Demo
-      </a>
-    );
-    const dnCode = (
-      <a href="https://github.com/Ihab-alazzawi/dev-Network" target="_blank">
-        Code
-      </a>
-    );
-    const pt = (
-      <a href="https://ihab.io" target="_blank">
-        Demo
-      </a>
-    );
-    const ptCode = (
-      <a href="https://github.com/Ihab-alazzawi/portfolio" target="_blank">
-        Code
-      </a>
-    );
-    const w = (
-      <a href="https://weather-by-city.herokuapp.com" target="_blank">
-        Demo
-      </a>
-    );
-    const wCode = (
-      <a
-        href="https://github.com/Ihab-alazzawi/Weather-app-React"
-        target="_blank"
-      >
-        Code
-      </a>
-    );
-
-    const images = [
-      {
-        original: devNetwork,
-        thumbnail: devNetwork,
-        description: [dn, ` - `, dnCode],
-        useTranslate3D: false
-      },
-      {
-        original: portfolio,
-        thumbnail: portfolio,
-        description: [pt, ` - `, ptCode],
-        useTranslate3D: false
-      },
-      {
-        original: weather,
-        thumbnail: weather,
-        description: [w, ` - `, wCode],
-        useTranslate3D: false
-      }
-    ];
-    return (
-      <div className="projects-main">
-        <h1>Projects</h1>
-        <p />
-        <ImageGallery items={images} useTranslate3D={images.useTranslate3D} />
+const Projects = () => (
+  <div className="projects-container">
+    <h1>Projects</h1>
+    <p />
+    <div className="box-container">
+      <div className="box-item">
+        <div className="flip-box">
+          <div className="flip-box-front text-center">
+            <div className="inner">
+              <h3 className="flip-box-header">Dev Network</h3>
+              <p className="line" />
+              <p className="bold">SOCIAL NETWORK FOR DEVELOPERS</p>
+              <p>
+                React - Redux - Node - Express - MongoDB - BootStrap - PWA -
+                Responsive
+              </p>
+              <i className="fas fa-arrow-circle-left fa-4x" />
+            </div>
+          </div>
+          <div className="flip-box-back text-center">
+            <div className="inner">
+              <a
+                href="https://dnetwork.herokuapp.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flip-box-button-top hover-top"
+              >
+                Live Demo
+              </a>
+              <a
+                href="https://github.com/Ihab-alazzawi/dev-Network"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flip-box-button-bottom hover-bottom"
+              >
+                View Code
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
-    );
-  }
-}
+      <div className="box-item">
+        <div className="flip-box">
+          <div className="flip-box-front text-center">
+            <div className="inner">
+              <h3 className="flip-box-header">Creative Portfolio</h3>
+              <p className="line" />
+              <p className="bold">PERSONAL PORTFOLIO</p>
+              <p>React - Pure CSS - PWA - Responsive</p>
+              <i className="fas fa-arrow-circle-left fa-4x" />
+            </div>
+          </div>
+          <div className="flip-box-back text-center">
+            <div className="inner">
+              <a
+                href="https://ihab.io"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flip-box-button-top hover-top"
+              >
+                Live Demo
+              </a>
+              <a
+                href="https://github.com/Ihab-alazzawi/portfolio"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flip-box-button-bottom hover-bottom"
+              >
+                View Code
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="box-item">
+        <div className="flip-box">
+          <div className="flip-box-front text-center filter-">
+            <div className="inner">
+              <h3 className="flip-box-header">Weather App</h3>
+              <p className="line" />
+              <p className="bold">WEATHER FINDER</p>
+              <p>React - Pure CSS - PWA - Responsive</p>
+              <i className="fas fa-arrow-circle-left fa-4x" />
+            </div>
+          </div>
+          <div className="flip-box-back text-center">
+            <div className="inner">
+              <a
+                href="https://weather-by-city.herokuapp.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flip-box-button-top hover-top"
+              >
+                Live Demo
+              </a>
+              <a
+                href="https://github.com/Ihab-alazzawi/Weather-app-React"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flip-box-button-bottom hover-bottom"
+              >
+                View Code
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
 
 export default Projects;
